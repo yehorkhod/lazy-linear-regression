6e7c4f71f7e6c306f41873d4e825ac57364f8111
yehor <e.khodysko@gmail.com>
2024-02-26 15:00:06 +0200

    setup mistake fixed

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     classifiers=[
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent'
     ]
 )
